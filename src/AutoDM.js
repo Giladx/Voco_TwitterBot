854d455d13d509a49928e23b875fd1f3d06808c9
Gilad Levi <gilad.x@gmail.com>
2018-08-09 15:54:57 +0300

    link changed
    changed to telegram link

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for following Voco Network. You are welcome to visit https://voconet.io.\n anytime 😊  \n Happy ${dayName} 😊😊 `; // your message
+  return `Hi ${name} Thanks for following Voco Network. You are welcome to visit https://t.me/VocoToken.\n anytime 😊  \n Happy ${dayName} 😊😊 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
